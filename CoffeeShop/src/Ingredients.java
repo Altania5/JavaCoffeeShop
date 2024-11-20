43a839d062c5f7657940c26e58cd01a11a5d7361
Alexander Konopelski <ak998863@wcupa.edu>
2024-11-20 16:19:22 -0500

    modifying ingredient tab

--- CoffeeShop/src/Ingredients.java
+++ CoffeeShop/src/Ingredients.java
@@ -4,6 +4,25 @@ import java.util.Map;
 
 public class Ingredients {
     private Connection connection;
+    private Map<String, IngredientData> ingredientMap;
+
+    public static class IngredientData {
+        public boolean inStock;
+        public String type;
+
+        public IngredientData(boolean inStock, String type) { // Constructor!
+            this.inStock = inStock;
+            this.type = type;
+        }
+
+        public boolean isInStock() { // or omit public for package-private
+            return inStock;
+        }
+
+        public String getType() {  // or omit public for package-private
+            return type;
+        }
+    }
 
     public Ingredients(Connection connection) {
         this.connection = connection;
@@ -33,27 +52,43 @@ public class Ingredients {
         }
     }
 
-    public void updateIngredientStock(String ingredientName, boolean inStock) {
+    public void updateIngredientStock(String ingredientName, boolean inStock, String type) { // Add type parameter
         try (PreparedStatement statement = connection.prepareStatement(
-                "UPDATE user.ingredients SET inStock = ? WHERE ingredients = ?")) {
+                "UPDATE user.ingredients SET inStock = ?, type = ? WHERE ingredients = ?")) {
             statement.setInt(1, inStock ? 1 : 0);
-            statement.setString(2, ingredientName);
+            statement.setString(2, type); // Update type
+            statement.setString(3, ingredientName);
             statement.executeUpdate();
         } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
-    public Map<String, Boolean> getIngredientList() {
-        Map<String, Boolean> ingredientList = new HashMap<>();
+    public void addIngredient(String ingredientName, boolean inStock, String type) { // Add type parameter
+        try (PreparedStatement statement = connection.prepareStatement(
+                "INSERT INTO user.ingredients (ingredients, inStock, type) VALUES (?, ?, ?)")) {
+            statement.setString(1, ingredientName);
+            statement.setInt(2, inStock ? 1 : 0);
+            statement.setString(3, type); // Set the type
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace(); // Always handle exceptions!
+        }
+    }
+
+    public Map<String, IngredientData> getIngredientList() { // Return Map<String, IngredientData>
+        Map<String, IngredientData> ingredientMap = new HashMap<>();
         try (Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery("SELECT * FROM user.ingredients")) {
             while (resultSet.next()) {
                 String name = resultSet.getString("ingredients");
                 boolean inStock = resultSet.getInt("inStock") == 1;
-                ingredientList.put(name, inStock);
+                String type = resultSet.getString("type");
+                ingredientMap.put(name, new IngredientData(inStock, type)); // Create IngredientData object
             }
         } catch (SQLException e) {
+            e.printStackTrace();
         }
-        return ingredientList;
+        return ingredientMap;
     }
 }
